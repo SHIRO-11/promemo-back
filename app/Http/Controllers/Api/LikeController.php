a7051d18054ff6cf9334b2a2bb6d4f2687cf1fda
Keisuke Tomiyoshi <keisuke.tomiyoshi@tam-bourine.co.jp>
2021-07-05 09:15:54 +0900

    add: コメント機能追加（途中）

--- app/Http/Controllers/Api/LikeController.php
+++ app/Http/Controllers/Api/LikeController.php
@@ -1,30 +1,58 @@
 <?php
 
-namespace App\Http\Controllers\api;
+namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\Post;
 use App\Models\Like;
+use App\Models\Post;
 use Auth;
 
-class LikeController extends Controller
-{
-     public function store(post $post)
-     {
-          $user = Auth::guard('sanctum')->user();
-          if ($user->id != $post->user_id) {
-               if ($post->isLiked(Auth::id())) {
-                    $delete_record = $post->getLike($user->id);
-                    $delete_record->delete();
-               } else {
-                    $like = Like::firstOrCreate(
-                         array(
-                              'user_id' => Auth::guard('sanctum')->user()->id,
-                              'post_id' => $post->id
-                         )
-                    );
-               }
-          }
+class LikeController extends Controller{
+
+    //コンポーネント初期読み込み時(created)に呼び出される
+    public function firstcheck($post) {
+     $auth = Auth::guard('sanctum')->user();
+     $likes = new Like();
+     $like = Like::where('posts_id',$post)->where('user_id',$auth->id)->first();
+     if($like) {
+          $count = $likes->where('posts_id',$post)->where('like',1)->count();
+          return [$like->like,$count];
+     } else {
+          $like = $likes->create([
+               'user_id' => $auth->id,
+               'posts_id' => $post,
+               'like' => 0
+          ]);
+          $count = $likes->where('posts_id',$post)->where('like',1)->count();
+          return response()->json([
+               'like' => $like->like,
+               'count' => $count
+          ]);
      }
+    }
+
+    //いいねボタンを押した時に呼び出される
+    public function check($post) {
+     $auth = Auth::guard('sanctum')->user();
+     $likes = new Like();
+     $like = Like::where('posts_id',$post)->where('user_id',$auth->id)->first();
+     if($like->like == 1) {
+          $like->like = 0;
+          $like->save();
+          $count = $likes->where('posts_id',$post)->where('like',1)->count();
+          return response()->json([
+               'like' => $like->like,
+               'count' => $count
+          ]);
+     } else {
+          $like->like = 1;
+          $like->save();
+          $count = $likes->where('posts_id',$post)->where('like',1)->count();
+          return response()->json([
+               'like' => $like->like,
+               'count' => $count
+          ]);
+     };
+    }
 }
